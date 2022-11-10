59e75284ebf20825a7d85e459f6b522aa70bf807
sushmitha01 <yelamakondasushmitha01@gmail.com>
2022-11-09 21:13:23 -0600

    updated the files to git

--- routes/Student.js
+++ routes/Student.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Student', { title: 'Search Results Students' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('Student', { title: 'Search Results Students' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+var express = require('express'); 
+const Student_controlers= require('../controllers/Student'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', Student_controlers.Student_view_all_Page ); 
+module.exports = router; 
